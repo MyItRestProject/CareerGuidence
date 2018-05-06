d6f56c69b22fb87af44d93d1fd910811a072ac90
Ilya <rickmortyand4@gmail.com>
2018-05-06 19:16:46 +0300

    Users

--- CareerGuidenceDB.sql
+++ CareerGuidenceDB.sql
@@ -1,2 +1,24 @@
 drop database IF EXISTS CareerGuidenceDB;
 create database CareerGuidenceDB;
+
+CREATE TABLE users(
+	id_user       INT(10) UNIQUE NOT NULL PRIMARY KEY,
+    age_user      INT(10),
+    surname_user  VARCHAR(255),
+    name_user     VARCHAR(255),
+    email_user    VARCHAR(255),
+    login_user    VARCHAR(255),
+    password_user VARCHAR(255)
+);
+
+CREATE TABLE roles(
+	id_roles   INT(10) UNIQUE NOT NULL PRIMARY KEY,
+	name_roles VARCHAR(255)
+);
+
+CREATE TABLE usersHasRoles(
+	id_user INT(10)      UNIQUE NOT NULL,
+    id_role INT(10)	     UNIQUE NOT NULL,
+    FOREIGN KEY(id_user) REFERENCES usersTable(id_user),
+    FOREIGN KEY(id_role) REFERENCES rolesTable(id_roles)
+)
