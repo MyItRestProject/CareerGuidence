1433d690926bd75a2b95f5944085c419cba3f4e8
Ilya <rickmortyand4@gmail.com>
2018-05-06 19:39:39 +0300

    Vanya works ,enterprises,interprise_has_work

--- CareerGuidenceDB.sql
+++ CareerGuidenceDB.sql
@@ -42,3 +42,26 @@ CREATE table answers(
     Flag bit (1)NOT NULL ,
     Primary key(id_specializes)
 );
+
+create table works (
+    name        varchar(100) NOT NULL,
+    position    varchar(100) NOT NULL,
+    id          int(10) NOT NULL unique,
+    primary key(id),
+    foreign key (id) references professions(id_profession)
+);
+
+create table enterprises (
+    name        varchar(100) NOT NULL,
+    phone       varchar(100) NOT NULL,
+    geolacation varchar(100) NOT NULL,
+    id          int(10) NOT NULL UNIQUE,
+    primary key(id)
+);
+
+create table interprise_has_work (
+    id_enterprise int(10) NOT NULL UNIQUE,
+    id_woks       int(10) NOT NULL UNIQUE,
+    foreign key (id_enterprise) references interprise(id),
+    foreign key (id_woks) references works(id)
+);
